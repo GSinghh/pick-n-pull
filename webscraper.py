a88d1080ee70b02c496e62b989cad5a22ce554b0
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-04-05 00:08:32 -0700

    Created PickNPull Class

--- webscraper.py
+++ webscraper.py
@@ -3,12 +3,23 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = Options()
-options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-driver.get("https://picknpull.com/check-inventory/")
+class PickNPull:
+    def __init__(self, make, model, start_year, end_year, postal_code, distance) -> None:
+        self.start_year = start_year
+        self.end_year = end_year
+        self.postal_code = postal_code
+        self.distance = distance
+        self.models = {}
+        self.makes = {}
+        
+        options = Options()
+        options.add_argument('--headless=new')
+        options.add_experimental_option("detach", True)
 
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    
 
-
-# models = {}
+    def get_makes_and_models(self):
+        self.driver.get("https://picknpull.com/check-inventory/")
