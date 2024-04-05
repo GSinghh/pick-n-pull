0cd3a2febca8c2189010e87bcc3b162ed93abb3a
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-04-04 23:37:37 -0700

    Web Scraper Testing For Pick N Pull

---
+++ webscraper.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+driver.get("https://picknpull.com/check-inventory/")
+
+
+
+# models = {}
