f54d5e75784a1430825c780117181f273d33bc15
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-04-06 22:55:31 -0700

    Finished get_makes function

--- webscraper.py
+++ webscraper.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -20,6 +22,23 @@ class PickNPull:
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     
     
-
-    def get_makes_and_models(self):
-        self.driver.get("https://picknpull.com/check-inventory/")
+    # This function will grab each brand from the drop down
+    # Stores them in the makes hashmap
+    # Remove {'Choose Model: 0}
+    def get_makes(self):
+        driver = self.driver
+        driver.get("https://picknpull.com/check-inventory/")
+        brands = Select(driver.find_element(By.XPATH, '//*[@id="main-content"]/div[1]/app-vehicle-search-controls/div/div/div/div[1]/div[1]/select'))
+        for brand in brands.options:
+            self.makes[brand.text] = brand.get_attribute('value')
+        print(self.makes)
+        
+        
+    # This function will grab each model from the drop down
+    # Stores them in the models hashmap
+    def get_models(self):
+        driver = self.driver
+        driver.get("https://picknpull.com/check-inventory/")
+        
+        
+ 
