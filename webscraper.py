deaf6e70a95b23d278bc0b07cb7e5e9b0550d9f1
GSinghh <50472056+GSinghh@users.noreply.github.com>
2024-04-21 23:52:55 -0700

    Storing Brand and Number from first dropdown

--- webscraper.py
+++ webscraper.py
@@ -4,7 +4,10 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
 
 class PickNPull:
     def __init__(self, make, model, start_year, end_year, postal_code, distance) -> None:
@@ -22,23 +25,44 @@ class PickNPull:
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     
     
-    # This function will grab each brand from the drop down
-    # Stores them in the makes hashmap
-    # Remove {'Choose Model: 0}
-    def get_makes(self):
+    # This function will grab each brand and value from the drop down
+    # Stores them in the makes dictionary
+    def get_makes_and_models(self):
+        #Initializing driver variable and grabbing URL 
         driver = self.driver
         driver.get("https://picknpull.com/check-inventory/")
-        brands = Select(driver.find_element(By.XPATH, '//*[@id="main-content"]/div[1]/app-vehicle-search-controls/div/div/div/div[1]/div[1]/select'))
+        
+        ignored_exceptions = (StaleElementReferenceException, NoSuchElementException)
+        wait = WebDriverWait(driver, 10, ignored_exceptions)
+        # brands = Select(wait.until(EC.visibility_of_element_located(By.XPATH, '//*[@id="main-content"]/div[1]/app-vehicle-search-controls/div/div/div/div[1]/div[1]/select'))
+        brands = wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="main-content"]/div[1]/app-vehicle-search-controls/div/div/div/div[1]/div[1]/select')))
+        brands = Select(brands)
         for brand in brands.options:
-            self.makes[brand.text] = brand.get_attribute('value')
-        print(self.makes)
+            # Since there is a placeholder value within the first option of the list
+            # We ignore that with this condition
+            brand_val = brand.get_attribute('value')
+            if brand_val != "0":
+                self.makes[brand.text] = brand_val
+                # Select the brand from dropdown
+                brands.select_by_value(brand_val)
+                
+                # Re-locate models dropdown after selecting the brand
+                models = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/app-root/div/div/div/app-check-inventory/app-home/div/div/div[1]/app-vehicle-search-controls/div/div/div/div[1]/div[2]/select')))
+                models = Select(models)
+                
+                # for model in models.options:
+                #     model_val = model.get_attribute('value')
+                #     if model_val != "0":
+                #         print(f"This is the model_val: {model_val}")
+                        
+                #         print(f"This is the model name: {model.text}")
+            
+            
+            
+            
+           
         
         
-    # This function will grab each model from the drop down
-    # Stores them in the models hashmap
-    def get_models(self):
-        driver = self.driver
-        driver.get("https://picknpull.com/check-inventory/")
         
-        
- 
+test = PickNPull("Acura", "Integra", "94", "01", 94560, 50)
+test.get_makes_and_models()
